5d5ff3fb776ebc307042acb5ec33b8ede5cda8fd
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-29 21:37:10 +0530

    react

--- react1/src/App.js
+++ react1/src/App.js
@@ -8,19 +8,19 @@ function App() {
 	<>
 		<div className="cardContainer">
 		<div className="card">
-		<Planet size={220} mood="happy" color="#FCCB7E" />
+		<Planet size={220} mood="happy" color="#FCCB7E" className="animate__animated animate__swing" />
 		</div>
 		<div className="card">
-		<Ghost size={240} mood="blissful" color="#E0E4E8" />
+		<Ghost size={240} mood="blissful" color="#E0E4E8" className="animate__animated animate__headShake"  />
 		</div>
 		<div className="card">
-		<Cat size={320} mood="excited" color="#596881" />
+		<Cat size={320} mood="excited" color="#596881" className="animate__animated animate__wobble"  />
 		</div>
 		<div className="card">
-		<CreditCard size={200} mood="sad" color="#83D1FB" />
+		<CreditCard size={200} mood="sad" color="#83D1FB" className="animate__animated animate__pulse"  />
 		</div>
 		<div className="card">
-		<Backpack size={320} mood="excited" color="#FFD882" />
+		<Backpack size={320} mood="excited" color="#FFD882" className="animate__animated animate__bounce" />
 		</div>
 		</div>
 	</>
