697a2f62f8047db4b20b80210b5df74bf30cbf6f
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-29 21:08:35 +0530

    react cat

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Planet, Ghost} from 'react-kawaii';
+import {Planet, Ghost, Cat, CreditCard} from 'react-kawaii';
 
 function App() {
   return (
