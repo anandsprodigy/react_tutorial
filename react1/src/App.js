1884e5289666c3aae26001a9ace02935080ec9b8
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-29 21:18:05 +0530

    react

--- react1/src/App.js
+++ react1/src/App.js
@@ -6,11 +6,21 @@ import {Planet, Ghost, Cat, CreditCard, Backpack} from 'react-kawaii';
 function App() {
   return (
 	<>
+		<div className="card">
 		<Planet size={220} mood="happy" color="#FCCB7E" />
+		</div>
+		<div className="card">
 		<Ghost size={240} mood="blissful" color="#E0E4E8" />
+		</div>
+		<div className="card">
 		<Cat size={320} mood="excited" color="#596881" />
+		
+		<div className="card">
 		<CreditCard size={200} mood="sad" color="#83D1FB" />
+		</div>
+		<div className="card">
 		<Backpack size={320} mood="excited" color="#FFD882" />
+		</div>
 	</>
   );
 }
