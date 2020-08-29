142a573986a2d5abd892ec5ed4b07937617edfa0
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-29 21:05:38 +0530

    comp ghost

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Planet} from 'react-kawaii';
+import {Planet, Ghost} from 'react-kawaii';
 
 function App() {
   return (
 	<>
 		<Planet size={220} mood="happy" color="#FCCB7E" />
+		<Ghost size={240} mood="blissful" color="#E0E4E8" />
+		
 	</>
   );
 }
