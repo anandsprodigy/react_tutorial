c0630511357a1651ee601be122c5a635b3eaecad
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-30 17:52:37 +0530

    AMD

--- react2/src/App.js
+++ react2/src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
         <Motherboard manufacturer="Intel" core="2" RAM="4" type="ATX" slot="PCI-e"/>
         <Motherboard manufacturer="AMD" core="2" RAM="8" type="iTX" slot="PCI-e"/>
+        <Motherboard manufacturer="Intel" core="4" RAM="8" type="micro-ATX" slot="PCI-e"/>
     </div>
   );
 }
