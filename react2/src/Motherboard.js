23a477728a620f498b25730d342fea708f46770f
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-30 17:21:16 +0530

    react2

--- react2/src/Motherboard.js
+++ react2/src/Motherboard.js
@@ -3,6 +3,14 @@ import React from 'react';
 
 class Motherboard extends React.Component{
 
+	constructor(props){
+		super(props);
+		this.state={
+			manufacturer=props.manufacturer,
+			core=props.core,
+			RAM=props.RAM
+		};
+	}
 
-	
+
 }
