01e519031ec0a0c08041a5ae2ac592f72c9a550f
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-31 17:34:17 +0530

    semantic

--- react4/src/ComposerSideBar.js
+++ react4/src/ComposerSideBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
-const SidebarExampleVisible = () => (
+const ComposerSideBar = () => (
   <Sidebar.Pushable as={Segment}>
     <Sidebar
       as={Menu}
@@ -55,4 +55,4 @@ const SidebarExampleVisible = () => (
   </Sidebar.Pushable>
 )
 
-export default SidebarExampleVisible
+export default ComposerSideBar;
