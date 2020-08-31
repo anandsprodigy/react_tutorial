570d9511ee6cea35b8e24fdd97f6c8156076a7dc
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-08-31 18:00:48 +0530

    react

--- react4/src/ComposerSideBar.js
+++ react4/src/ComposerSideBar.js
@@ -1,58 +1,29 @@
-import React from 'react'
-import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import React from 'react';
 
-const ComposerSideBar = () => (
-  <Sidebar.Pushable as={Segment}>
-    <Sidebar
-      as={Menu}
-      animation='overlay'
-      icon='labeled'
-      inverted
-      vertical
-      visible
-      width='thin'
-    >
-      <Menu.Item as='a'>
-        <Icon name='home' />
-        Home
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='Motherboard' />
-        Motherboard
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='camera' />
-        Processor
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='microchip' />
-        RAM
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='camera' />
-        Graphics Memory
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='camera' />
-        Storage
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='camera' />
-        Cabinet
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='camera' />
-        Power Supply
-      </Menu.Item>
-    </Sidebar>
+function ComposerSideBar(){
+  return(
 
-    <Sidebar.Pusher>
-      <Segment basic>
-        <Header as='h3'>Application Content</Header>
-        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-      </Segment>
-    </Sidebar.Pusher>
-  </Sidebar.Pushable>
-)
+    <>
+      <div className="sidebar">
+        <div className="sidebarMenu">
+          <p>MotherBoard</p>
+        </div>
+        <div className="sidebarMenu">
+          <p>RAM</p>
+        </div>
+        <div className="sidebarMenu">
+          <p>Graphics</p>
+        </div>
+        <div className="sidebarMenu">
+          <p>Cabinet</p>
+        </div>
+        <div className="sidebarMenu">
+          <p>Power</p>
+        </div>
+      </div>
+    </>
+
+  );
+}
 
 export default ComposerSideBar;
