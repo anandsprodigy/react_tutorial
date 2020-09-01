925e3eb97f6fdb82449e11d2d3904c5f5764a50e
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-09-01 22:45:02 +0530

    demo4

--- react3/src/App.js
+++ react3/src/App.js
@@ -18,6 +18,17 @@ function App() {
     <Button variant="primary">Go somewhere</Button>
   </Card.Body>
 </Card>
+
+ <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>
+          Change this and that and try again. Duis mollis, est non commodo
+          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+          Cras mattis consectetur purus sit amet fermentum.
+        </p>
+      </Alert>
+
+
     </div>
   );
 }
