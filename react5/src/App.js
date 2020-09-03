5f8a3baf4f2a2ceea1e5d591070a44bba8f2bd44
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-09-03 13:58:49 +0530

    d

--- react5/src/App.js
+++ react5/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Card, Button } from 'react-bootstrap';
+import  {  Card,
+  CardHeader,
+  CardTitle,
+  CardImg,
+  CardBody,
+  CardFooter,
+  Button}  from 'react-bootstrap';
 
 function App() {
-    return ( <
-     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of
-          the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    return ( 
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/9/9a/AVADirect-Custom-X99-Intel-Core-i7-gaming-cpu.png" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     );
 }
 
