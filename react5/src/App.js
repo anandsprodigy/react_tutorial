b3e0a8a55021d3e9679aae4d1939208c7c5d1e7e
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-09-05 00:14:14 +0530

    title

--- react5/src/App.js
+++ react5/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/9/9a/AVADirect-Custom-X99-Intel-Core-i7-gaming-cpu.png" />
         <Card.Body>
-          <Card.Title>AMD-YFT-001</Card.Title>
+          <Card.Title className="text-center">AMD-YFT-001</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the bulk of
             the card's content.
