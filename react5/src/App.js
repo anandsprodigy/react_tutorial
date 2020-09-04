8758c9c25a141048005a0c3e7cbf11379af59e29
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-09-04 23:47:27 +0530

    d

--- react5/src/App.js
+++ react5/src/App.js
@@ -7,18 +7,23 @@ import  {  Card,
   CardImg,
   CardBody,
   CardFooter,
-  Button}  from 'react-bootstrap';
+  Button, ListGroup}  from 'react-bootstrap';
 
 function App() {
     return ( 
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/9/9a/AVADirect-Custom-X99-Intel-Core-i7-gaming-cpu.png" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>AMD-YFT-001</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the bulk of
             the card's content.
           </Card.Text>
+          <ListGroup variant="flush">
+            <ListGroup.Item>AMD RYZEN 3 3100</ListGroup.Item>
+            <ListGroup.Item>ASRock 450M Pro4</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
