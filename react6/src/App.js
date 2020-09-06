1e92cf81d728f323c717be18f0823ef4c78e364d
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-09-06 12:18:41 +0530

    demo

--- react6/src/App.js
+++ react6/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReadyComponent from './ReadyComponent';
+import ReadyComponentGET from './ReadyComponentGET';
 
 function App() {
   return (
+    <>
     <div className="App">
       <ReadyComponent />
     </div>
+    <div>
+      <ReadyComponentGET />
+    </div>
+    </>
   );
 }
 
---
+++ react6/src/ReadyComponentGET.js
@@ -0,0 +1,36 @@
+// Ready component is to diplay ready configurations of the PC
+import React, { Component } from 'react';
+import './ReadyComponent.css';
+import IntelLogo from './images/logos/intel-header-logo.svg';
+import AMDLogo from './images/logos/AMD.svg';
+import MotherboardLogo from './images/logos/motherboard.png';
+import CPULogo from './images/logos/cpu.png';
+import GraphicsCardLogo from './images/logos/graphic-card.png';
+import PowerSupplyLogo from './images/logos/power-supply.png';
+import RAMLogo from './images/logos/ram.png';
+import StorageSSDLogo from './images/logos/ssd.png';
+import StorageHDDLogo from './images/logos/harddrive.png';
+import CabinetLogo from './images/logos/hardware.png';
+import axios from 'axios';
+
+export default class ReadyComponentGET extends Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.persons.map(person => <li>{person.name}</li>)}
+      </ul>
+    )
+  }
+}
